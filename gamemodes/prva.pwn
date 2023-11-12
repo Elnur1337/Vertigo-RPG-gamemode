cefb88f3baf701183b5d37f1c5b056f7c3d2dd10
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2023-11-12 20:10:34 +0100

    Added streamer plugin and created first cmd

--- gamemodes/prva.pwn
+++ gamemodes/prva.pwn
@@ -27,3 +27,30 @@ public GetCharFromPlayerNameAt(playerid, index)
 	return name[index];
 
 }
+
+// => Pickups
+forward _CreateDynamicPickup(modelid, type, Float:x, Float:y, Float:z, worldid, interiorid, playerid, streamdistance, areaid, priority);
+public _CreateDynamicPickup(modelid, type, Float:x, Float:y, Float:z, worldid, interiorid, playerid, streamdistance, areaid, priority) { return CreateDynamicPickup(modelid, type, x, y, z, worldid, interiorid, playerid, streamdistance, areaid, priority); }
+
+forward _DestroyDynamicPickup(STREAMER_TAG_PICKUP:pickupid);
+public _DestroyDynamicPickup(STREAMER_TAG_PICKUP:pickupid) { return DestroyDynamicPickup(STREAMER_TAG_PICKUP:pickupid); }
+
+forward _IsValidDynamicPickup(STREAMER_TAG_PICKUP:pickupid);
+public _IsValidDynamicPickup(STREAMER_TAG_PICKUP:pickupid) { return IsValidDynamicPickup(STREAMER_TAG_PICKUP:pickupid); }
+
+// => 3D Text Labels
+forward _CreateDynamic3DTextLabel(text[], color, Float:x, Float:y, Float:z, Float:drawdistance);
+public _CreateDynamic3DTextLabel(text[], color, Float:x, Float:y, Float:z, Float:drawdistance) { return CreateDynamic3DTextLabel(text, color, Float:x, Float:y, Float:z, Float:drawdistance); }
+
+forward _DestroyDynamic3DTextLabel(STREAMER_TAG_3D_TEXT_LABEL:id);
+public _DestroyDynamic3DTextLabel(STREAMER_TAG_3D_TEXT_LABEL:id) { return DestroyDynamic3DTextLabel(STREAMER_TAG_3D_TEXT_LABEL:id); }
+
+forward _IsValidDynamic3DTextLabel(STREAMER_TAG_3D_TEXT_LABEL:id);
+public _IsValidDynamic3DTextLabel(STREAMER_TAG_3D_TEXT_LABEL:id) { return IsValidDynamic3DTextLabel(STREAMER_TAG_3D_TEXT_LABEL:id); }
+
+forward _UpdateDynamic3DTextLabelText(STREAMER_TAG_3D_TEXT_LABEL:id, color, text[]);
+public _UpdateDynamic3DTextLabelText(STREAMER_TAG_3D_TEXT_LABEL:id, color, text[]) { return UpdateDynamic3DTextLabelText(STREAMER_TAG_3D_TEXT_LABEL:id, color, text); }
+
+// => Updates
+forward _samp_Streamer_Update(playerid);
+public _samp_Streamer_Update(playerid) { return Streamer_Update(playerid); }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const samp = require("samp-node-lib");
 
+const events = require("events");
+const CMD = new events.EventEmitter();
+
 const hash = require("md5");
 
 const database = require("./databaseFunctions/dbConfig");
@@ -35,13 +38,68 @@ let RegisterTextDraws = [];
 //PlayersInfo
 let players = [];
 
+/* For Streamer */
+const streamer = {
+    CreateDynamicPickup: function(modelid, type, x, y, z, worldid = -1, interiorid = -1, playerid = -1, streamdistance = 200.0, areaid = -1, priority = 0) {
+        var RETURN = samp.callPublic("_CreateDynamicPickup", "iifffiiifii", modelid, type, x, y, z, worldid, interiorid, playerid, streamdistance, areaid, priority);
+        this.UpdateAll();
+        return RETURN;
+    },
+    DestroyDynamicPickup: function(pickupid) {
+        var RETURN = samp.callPublic("_DestroyDynamicPickup", "i", pickupid);
+        this.UpdateAll();
+        return RETURN;
+    },
+    IsValidDynamicPickup: function(pickupid) {
+        return samp.callPublic("_IsValidDynamicPickup", "i", pickupid);
+    },
+    CreateDynamic3DTextLabel: function(text, color, x, y, z, drawdistance) {
+        var RETURN = samp.callPublic("_CreateDynamic3DTextLabel", "siffff", text, color, x, y, z, drawdistance);
+        this.UpdateAll();
+        return RETURN;
+    },
+    DestroyDynamic3DTextLabel: function(id) {
+        var RETURN = samp.callPublic("_DestroyDynamic3DTextLabel", "i", id);
+        this.UpdateAll();
+        return RETURN;
+    },
+    IsValidDynamic3DTextLabel: function(id) {
+        samp.callPublic("_IsValidDynamic3DTextLabel", "i", id);
+    },
+    UpdateDynamic3DTextLabelText: function(id, color, text) {
+        var RETURN = samp.callPublic("_UpdateDynamic3DTextLabelText", "iis", id, color, text);
+        this.UpdateAll();
+        return RETURN;
+    },
+    Update: function(playerid) {
+        return samp.callPublic("_samp_Streamer_Update", "i", playerid);
+    },
+    UpdateAll: function() {
+        samp.getPlayers().forEach((player) => {
+            this.Update(player.playerid);
+        });
+    }
+};
+
 samp.OnGameModeInit(() => { 
-    // samp.AddPlayerClass(0, 2095.5671, 1433.1622, 10.8203, 92.4388, 0, 0, 0, 0, 0, 0);
-    samp.AddPlayerClass(294, 401.5687, -1536.4258, 32.2734, 223.7568, 0, 0, 0, 0, 0, 0); 
-    samp.AddStaticVehicle(579, 401.5687, -1536.4258, 32.2734, 223.7568, -1, -1);
+    // samp.AddPlayerClass(294, 401.5687, -1536.4258, 32.2734, 223.7568, 0, 0, 0, 0, 0, 0); -- PRAVI
+    samp.AddPlayerClass(294,1465.5649,-1726.7954,13.5469,104.0015,0,0,0,0,0,0);
+    samp.AddStaticVehicle(411, 401.5687, -1533.4258, 32.2734, 223.7568, -1, -1);
     
     RegisterTextDrawsLoader(RegisterTextDraws);
 
+    samp.DisableInteriorEnterExits();
+    samp.EnableStuntBonusForAll(0);
+
+    streamer.CreateDynamicPickup(18749, 1, 409.3643, -1543.9668, 32.2734, 0);
+    streamer.CreateDynamic3DTextLabel(`{${darkGreenWithoutHash}}==================\n[Vertigo RPG]\n{${whiteWithoutHash}}Verzija: 1.0\nDeveloper: Elnur\n{${darkGreenWithoutHash}}==================`, white, 409.3643, -1543.9668, 32.2734, 8);
+
+    streamer.CreateDynamicPickup(1318, 1, 1481.0789, -1772.0646, 18.7958, 0);
+    streamer.CreateDynamic3DTextLabel(`{B7BA00}[OPSTINA]\n{${whiteWithoutHash}}Koristite F/ENTER za ulaz`, white, 1481.0789, -1772.0646, 19.7958, 8);
+    streamer.CreateDynamicPickup(1318, 1, 390.1459, 173.7937, 1008.3828, 1, 3);
+    streamer.CreateDynamic3DTextLabel(`{B7BA00}[OPSTINA]\n{${whiteWithoutHash}}Koristite F/ENTER za izlaz`, white, 390.1459, 173.7937, 1009.3828, 8);
+    
+    
     return true;
 });
 
@@ -60,7 +118,6 @@ samp.OnPlayerConnect((playerid) => {
 
     players[playerid].nickname = GetPlayerNameString(playerid);
     players[playerid].nickname = players[playerid].nickname.substring(0, players[playerid].nickname.length - 1);
-    console.log(players[playerid].nickname);
     try {
         const query = "SELECT id FROM players WHERE nickname = ?";
         database.query(query, [players[playerid].nickname], (err, res) => {
@@ -81,7 +138,7 @@ samp.OnPlayerConnect((playerid) => {
 
 samp.OnPlayerRequestClass((playerid) => {
     if (players[playerid].isRegistered) {
-        playerid.ShowPlayerDialog(loginDialog, samp.DIALOG_STYLE.PASSWORD, "Log in", `Dobrodosao {${whiteWithoutHash}}${players[playerid].nickname} {${dialogDefaultColorWithoutHash}}na {${darkGreenWithoutHash}}Vertigo RPG!\n{${dialogDefaultColorWithoutHash}}Molimo ukucajte vasu sifru`, "Unesi", "Izlaz");
+        playerid.ShowPlayerDialog(loginDialog, samp.DIALOG_STYLE.PASSWORD, "Login", `Dobrodosao {${whiteWithoutHash}}${players[playerid].nickname} {${dialogDefaultColorWithoutHash}}na {${darkGreenWithoutHash}}Vertigo RPG!\n{${dialogDefaultColorWithoutHash}}Molimo ukucajte vasu sifru`, "Unesi", "Izlaz");
     } else {
         //Clear TextDraws
         samp.TextDrawSetString(RegisterTextDraws[3].TextDraw, "Password");
@@ -114,6 +171,10 @@ samp.OnPlayerSpawn((playerid) => {
         setTimeout(() => {playerid.Kick();}, 10);
     }
     
+    setInterval(() => {
+        samp.SendClientMessageToAll(white, "Test Poruke");
+    }, 300000);
+
 	return true;
 });
 
@@ -288,6 +349,10 @@ samp.OnDialogResponse((playerid, dialogid, response, listitem, inputtext) => {
                                         players[playerid].pol = res[0].pol;
                                         players[playerid].godine = res[0].godine;
                                         players[playerid].drzava = res[0].selectedCountryId;
+                                        for (let i = 0; i < 200; i++) {
+                                            samp.SendClientMessage(playerid, white, "");
+                                        }
+                                        samp.SendClientMessage(playerid, white, `{${darkGreenWithoutHash}}[Vertigo RPG] {${whiteWithoutHash}}Dobrodos${players[playerid].pol == 'M' ? 'ao' : 'la'} na server, uzivaj u igri!`);
                                         playerid.SpawnPlayer();
                                     }
                                 });
@@ -297,7 +362,7 @@ samp.OnDialogResponse((playerid, dialogid, response, listitem, inputtext) => {
                             }
                         } else {
                             samp.SendClientMessage(playerid, darkGreen, `[Vertigo RPG] {${whiteWithoutHash}}Pogresna sifra, molimo pokusajte ponovo!`);
-                            playerid.ShowPlayerDialog(loginDialog, samp.DIALOG_STYLE.PASSWORD, "Log in", `Dobrodosao {${whiteWithoutHash}}${players[playerid].nickname} {${dialogDefaultColorWithoutHash}}na {${darkGreenWithoutHash}}Vertigo RPG!\n{${dialogDefaultColorWithoutHash}}Molimo ukucajte vasu sifru`, "Unesi", "Izlaz");
+                            playerid.ShowPlayerDialog(loginDialog, samp.DIALOG_STYLE.PASSWORD, "Login", `Dobrodosao {${whiteWithoutHash}}${players[playerid].nickname} {${dialogDefaultColorWithoutHash}}na {${darkGreenWithoutHash}}Vertigo RPG!\n{${dialogDefaultColorWithoutHash}}Molimo ukucajte vasu sifru`, "Unesi", "Izlaz");
                             ++(players[playerid].wrongPassRepeat);
                             if (players[playerid].wrongPassRepeat >= 4) {
                                 samp.SendClientMessage(playerid, darkRed, `[KICK] {${whiteWithoutHash}}Pogrijesili ste sifru cetiri puta!`);
@@ -316,6 +381,41 @@ samp.OnDialogResponse((playerid, dialogid, response, listitem, inputtext) => {
     }
 });
 
+samp.OnPlayerKeyStateChange((playerid, newkeys, oldkeys) => {
+    if (newkeys & samp.KEY.SECONDARY_ATTACK) {
+        if (samp.IsPlayerInRangeOfPoint(playerid, 2, 1480.9833, -1772.1000, 18.7958)) {
+            samp.SetPlayerVirtualWorld(playerid, 1);
+            samp.SetPlayerInterior(playerid, 3);
+            samp.SetPlayerPos(playerid, 390.1459, 173.7937, 1008.3828);
+            samp.SetPlayerFacingAngle(playerid, 90);
+            samp.SetCameraBehindPlayer(playerid);
+        } else if (samp.IsPlayerInRangeOfPoint(playerid, 2, 390.1459, 173.7937, 1008.3828)) {
+            samp.SetPlayerVirtualWorld(playerid, 0);
+            samp.SetPlayerInterior(playerid, 0);
+            samp.SetPlayerPos(playerid, 1480.9833, -1772.1000, 18.7958);
+            samp.SetPlayerFacingAngle(playerid, 0);
+            samp.SetCameraBehindPlayer(playerid);
+        }
+        
+    }
+});
+
+samp.OnPlayerCommandText((playerid, cmdtext) => {
+    if (players[playerid].isLoggedIn) {
+        cmdtext = cmdtext.toLowerCase();
+        cmdtext = cmdtext.replace("/", "");
+        let params = cmdtext.split(/[ ]+/);
+        let cmdName = params[0];
+        if(CMD.eventNames().some(s => s == cmdName)) {
+            params.shift();
+            CMD.emit(`${cmdName}`, playerid, params);
+        } else {
+            console.log("Unesena je komanda koja ne postoji");
+        }
+        return true;
+    }
+});
+
 samp.OnPlayerDisconnect((playerid, reason) => {
     players[playerid] = {
         nickname: "",
@@ -327,4 +427,12 @@ samp.OnPlayerDisconnect((playerid, reason) => {
         isRegistered: false,
         isLoggedIn: false
     };
+});
+
+CMD.on('avozilo', (playerid, params) => {
+    params[0] = parseInt(params[0]);
+    pos = samp.GetPlayerPos(playerid);
+    let angle = samp.GetPlayerFacingAngle(playerid);
+    const veh = samp.CreateVehicle(params[0], pos[0], pos[1], pos[2], angle, -1, -1, 9999, 0);
+    samp.PutPlayerInVehicle(playerid, veh, 0);
 });
